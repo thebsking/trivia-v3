89f24425d5e38b1acad5103f9c8be379cf233db8
Brandon King <brandonscottking@gmail.com>
2022-07-10 19:54:54 -0500

    who knows whats happenign at this poitn

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 router.use('/api', apis);
 
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
