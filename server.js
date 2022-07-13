4b8166c31d48c063be4ea04ac48bd4a901a0dd6f
Brandon King <brandonscottking@gmail.com>
2022-07-13 15:12:06 -0500

    location admin page

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/trivia', {
+mongoose.connect( 'mongodb://localhost/trivia', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
